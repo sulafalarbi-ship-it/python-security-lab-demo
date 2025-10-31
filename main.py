616714ca2438cf071ada6fa49ff6644949cb3726
sulafalarbi-ship-it <sulafalarbi@gmail.com>
2025-10-31 22:18:43 +0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-import yaml
-
-def unsafe_command(input_str):
-    os.system(input_str) 
-with open('config.yaml', 'r') as f:
-    config = yaml.full_load(f) 
-unsafe_command(config.get('user_input', 'echo Hello'))
